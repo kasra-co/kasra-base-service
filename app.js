706334dc20517e327c9015a1328df5637d63d7b1
Tamas Geschitz <getamas@gmail.com>
2015-08-17 14:56:12 +0200

    Added new print() function

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 var express = require( 'express' ),
 	middleware = require( './middleware' ),
-	acceslog = require( 'morgan' );
+	acceslog = require( 'morgan' ),
+	tools = require( './lib/tools' );
 
 module.exports = {
 	create: function createBaseApp(args) {
@@ -12,6 +13,7 @@ module.exports = {
 		app.set('logger',args.logger);
 		app.set('db',args.db);
 		app.set('config',args.config);
+		app.set('tools',tools);
 		app.use( acceslog( 'dev' ));
 		// Add all common middleware here (except for 404)
 		app.use( middleware.common.accpetJSON);
